fbc343b2f64aead765a1133459851d00da5804ca
Navikaran kumar <73513161+navikaran1@users.noreply.github.com>
2024-08-08 22:11:00 +0530

    Add files via upload

---
+++ Wifi_password-3.bat
@@ -0,0 +1,20 @@
+@echo off
+:: Get the name of the connected Wi-Fi network
+for /f "tokens=2 delims=:" %%i in ('netsh wlan show interfaces ^| findstr "^....SSID"') do set SSID=%%i
+set SSID=%SSID:~1%
+
+:: Retrieve and display the Wi-Fi password for the connected network
+echo Current Wi-Fi Network Password:
+netsh wlan show profile name="%SSID%" key=clear | findstr Key
+
+:: Display additional details about the current connected network
+echo.
+echo Additional Wi-Fi Details:
+netsh wlan show interfaces
+
+:: Run a speed test using speedtest-cli
+echo.
+echo Running Speed Test...
+speedtest
+
+pause
